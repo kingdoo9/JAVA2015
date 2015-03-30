cf11fdd71b2de3a1f7097acdac37676504d5f659
kingdoo9 <cokr2343@naver.com>
2015-03-30 16:50:38 +0900

    DAO Refactoring

---
+++ 성적처리/src/DAOs/DAO.java
@@ -0,0 +1,37 @@
+package DAOs;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class DAO {
+	public Object read(String fileName) {
+		Object object = null;
+		try {
+			ObjectInputStream In;
+			In = new ObjectInputStream(new FileInputStream(fileName));
+			object = In.readObject();
+			In.close();
+			
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return object;
+	}
+	public void write(Object object, String fileName) {
+			
+		//serialize
+		try {
+			ObjectOutputStream Out  = 
+					new ObjectOutputStream(new FileOutputStream(fileName));
+			Out.writeObject(object);
+			Out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
